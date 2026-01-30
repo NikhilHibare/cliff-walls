88226957c8a7956fc068340fbe13e16eddc566d2
Unreliable Ally <83826301+NikhilHibare@users.noreply.github.com>
2026-01-30 11:52:17 +0530

    Update startupMessage.js

--- scripts/startupMessage.js
+++ scripts/startupMessage.js
@@ -1,1 +1,5 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
+// startupMessage.js - Just a simple test
+Hooks.once('ready', function() {
+  console.log("Cliff Walls module loaded");
+  // Your actual code here
+});
